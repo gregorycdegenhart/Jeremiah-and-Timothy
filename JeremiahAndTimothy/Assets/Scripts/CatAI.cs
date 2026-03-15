f14eaa39c067613cc9751dda3bad55ee25cce440
gregorycdegenhart <gregorycdegenhart@gmail.com>
2026-03-14 20:21:34 -0400

    Fixed Playable Build

--- JeremiahAndTimothy/Assets/Scripts/CatAI.cs
+++ JeremiahAndTimothy/Assets/Scripts/CatAI.cs
@@ -7,20 +7,12 @@ using UnityEngine.AI;
 
 public class CatAI : MonoBehaviour
 {
-
     public List<Transform> wayPoint;
-
     NavMeshAgent navMeshAgent;
-
     public int currentWayPointIndex = 0;
-
     public float lookRadius = 10f;
-
     public GameObject detectScreenUI;
-
     Transform target;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
     {
@@ -28,34 +20,30 @@ public class CatAI : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Walking();
-
         float distance = Vector3.Distance(target.position, transform.position);
 
         if (distance <= lookRadius)
         {
+            // Chase player
             navMeshAgent.SetDestination(target.position);
-
             detectScreenUI.SetActive(true);
 
             if (distance <= navMeshAgent.stoppingDistance)
             {
-                // Attack the target
                 FaceTarget();
-
             }
         }
-
         else
         {
+            // Patrol waypoints
             detectScreenUI.SetActive(false);
+            Walking();
         }
     }
 
-    void FaceTarget ()
+    void FaceTarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
@@ -64,14 +52,14 @@ public class CatAI : MonoBehaviour
 
     void Walking()
     {
-        if(wayPoint.Count == 0)
+        if (wayPoint.Count == 0)
         {
             return;
         }
 
         float distanceToWayPoint = Vector3.Distance(wayPoint[currentWayPointIndex].position, transform.position);
 
-        if(distanceToWayPoint <= 2)
+        if (distanceToWayPoint <= 2)
         {
             currentWayPointIndex = (currentWayPointIndex + 1) % wayPoint.Count;
         }
